913ba48a88fdd4cfac2bc964dbd31df2beb0a4e1
Jonathan Henrique <jonathan369852@gmail.com>
2024-02-21 22:49:03 -0300

    new
    mudança de layout

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function showHobbyInfo(hobby) {
 
 
 const dark_color = ['#171717', '#eeeedd', '#111111', '#333333', '#444444', '#aaaaaa' ]
-const light_color = ['#eeeedd', '#171717', '#c9c0c0', '#cccccc', '#bbbbbb', '#555555']
+const light_color = ['#e8e8e8', '#171717', '#c9c0c0', '#cccccc', '#bbbbbb', '#555555']
 
 document.querySelector('#toggle-btn').addEventListener('click', function () {
     if (this.checked) {
